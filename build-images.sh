cbd1bfc0bfcff9c4e71d705d630773915ae7cb0a
mrmarkuz <mrmarkuz666@gmail.com>
2025-04-16 20:25:58 +0200

    Repository initialization

--- build-images.sh
+++ build-images.sh
@@ -13,22 +13,22 @@ images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
-reponame="kickstart"
+reponame="headscale"
 
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-headscale container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-headscale; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-headscale -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-headscale \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/nginx:1.27.1-alpine3.20" \
+    --label="org.nethserver.images=docker.io/headscale/headscale:v0.25.1 ghcr.io/tale/headplane:0.5.10" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
