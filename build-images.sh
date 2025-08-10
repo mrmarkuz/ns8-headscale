de0124a8f72b54d81f5c03b80ade8758fb024eee
mrmarkuz <mrmarkuz666@gmail.com>
2025-08-10 21:45:00 +0200

    Second draft using headscale-ui instead headplane

--- build-images.sh
+++ build-images.sh
@@ -37,9 +37,9 @@ buildah add "${container}" ui/dist /ui
 
 buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
-    --label="org.nethserver.tcp-ports-demand=1" \
+    --label="org.nethserver.tcp-ports-demand=2" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/headscale/headscale:v0.25.1 ghcr.io/tale/headplane:0.5.10" \
+    --label="org.nethserver.images=docker.io/headscale/headscale:v0.26.1 ghcr.io/tale/headplane:0.6.0-1 ghcr.io/gurucomputing/headscale-ui:2025.07.12" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
